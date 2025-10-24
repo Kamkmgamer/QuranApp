86d8b8a91953248581a605c41ef5d50a3e91b97c
Kamkmgamer <khalilabdalmajeed@gmail.com>
2025-10-24 19:01:29 +0300

    Create NoRecentView for empty states

--- screens/home/components/NoRecentView.tsx
+++ screens/home/components/NoRecentView.tsx
@@ -2,8 +2,8 @@ import { Text } from "react-native";
 
 export function NoRecentView() {
   return (
-    <Text className="text-center font-HelveticaRoman text-primary/20 dark:text-primaryDark/70">
-      لم يتم قراءة شئ موخرا
+    <Text className="text-center font-HelveticaRoman text-textLight/70 dark:text-textLight/70">
+      لم يتم قراءة شئ مؤخرا
     </Text>
   );
 }
