746f0b05d495651bbc8b33abe01c3dbdf38071e0
Kamkmgamer <khalilabdalmajeed@gmail.com>
2025-10-24 19:01:31 +0300

    Implement Prayer component

--- screens/prayers/components/Prayer.tsx
+++ screens/prayers/components/Prayer.tsx
@@ -2,11 +2,11 @@ import { Text, View } from "react-native";
 
 export function Prayer({ title, value }: { title: string; value: string }) {
   return (
-    <View className="flex-row justify-between items-center bg-lotion dark:bg-blackCoral p-5 rounded-md mt-5">
-      <Text className="text-2xl font-HelveticaRoman text-primary dark:text-primaryDark">
+    <View className="flex-row justify-between items-center bg-white dark:bg-darkGreen/30 p-5 rounded-2xl mt-4 border border-darkGreen/20">
+      <Text className="text-xl font-HelveticaBold text-darkGreen dark:text-textLight">
         {title}
       </Text>
-      <Text className="text-xl font-HelveticaRoman text-primary dark:text-primaryDark">
+      <Text className="text-lg font-HelveticaRoman text-darkGreen dark:text-textLight">
         {value}
       </Text>
     </View>
